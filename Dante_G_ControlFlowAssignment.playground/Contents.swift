e5479d34a2264bd2b9605f80835d785256da48d0
DanteGivens <67976030+DanteGivens@users.noreply.github.com>
2020-07-14 15:45:37 -0400

    Add files via upload

---
+++ Dante_G_ControlFlowAssignment.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var TotalTimeforEgg = 180
+//180 is the time represented in seconds
+if TotalTimeforEgg < 180 {
+    print ("the egg is undercooked and runny leave it in the pan")
+}
+if TotalTimeforEgg > 180 {
+    print (" The egg is overcooked you might want to throw it away")
+}
+if TotalTimeforEgg == 180 {
+    print (" Perfect timing, now take the egg out of the pan")
+}
